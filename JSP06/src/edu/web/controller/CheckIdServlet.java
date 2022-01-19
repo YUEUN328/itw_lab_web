1389e8eae5e8a72c056d5aeb1803554a436211aa
YUEUN328 <yelub@naver.com>
2022-01-19 13:41:51 +0900

    .

--- JSP06/src/edu/web/controller/CheckIdServlet.java
+++ JSP06/src/edu/web/controller/CheckIdServlet.java
@@ -17,6 +17,7 @@ public class CheckIdServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		// 요청 파라미터들의 한글 처리
@@ -24,6 +25,7 @@ public class CheckIdServlet extends HttpServlet {
 		
 		// 요청 파라미터 값을 읽음
 		String userId = request.getParameter("userId");
+		System.out.println("userId = " + userId);
 		
 		// TODO: DB 테이블에 없는 아이디인지 체크
 		// admin, guest는 사용 불가능(invalid), 그 이외의 아이디들은 사용 가능(valid) 응답
